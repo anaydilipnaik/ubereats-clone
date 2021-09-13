02bac6ac2488d6f1243d2ee06d3c490b6f4e8a80
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2021-09-13 02:06:56 -0700

    db connected!

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,23 @@ var app = express();
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
+
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "ubereats-db.cz3xhefv1lvq.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "ubereatsadmin",
+  database: "ubereats",
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// var sql = "Select * from users";
+// con.query(sql, (err, res) => {
+//   console.log("err", err);
+//   console.log("res", res);
+// });
