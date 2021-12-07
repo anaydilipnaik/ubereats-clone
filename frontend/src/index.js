251cfde34696e35f6c152eb57eecd8d98a9fd3c1
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2021-12-07 00:58:57 -0800

    bug fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,14 @@ import { PersistGate } from "redux-persist/integration/react";
 import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { createHttpLink } from "apollo-link-http";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5002",
+});
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
