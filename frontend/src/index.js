08da02e7f7a67bc79c927d61da3c14fa332421aa
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2021-12-07 00:03:34 -0800

    frontend initial commit - packages installed and apollo client setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,22 @@ import App from "./App";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
